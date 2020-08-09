628d84c8d99a486e83ae8dd0a542ff8bf7893640
mgalarowicz <rezi610@gmail.com>
2020-08-09 17:07:55 +0200

    (1)

---
+++ Threading/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(DifferentMethod);
+            thread.Start();
+        }
+
+        static void DifferentMethod()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
