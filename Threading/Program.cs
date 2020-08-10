56336aa810376ebbd4b84c27d6b0b42d3b5f9e19
mgalarowicz <rezi610@gmail.com>
2020-08-10 20:55:27 +0200

    Divide and Conquer p1

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,26 +1,30 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 
 namespace Threading
 {
     class Program
     {
-        static object baton = new object();
+        static byte[] values = new byte[500000000];
+        static void GenerateInts()
+        {
+            var rand = new Random(987);
+            for (int i = 0; i < values.Length; i++)
+                values[i] = (byte)rand.Next(10);
+        }
 
         static void Main(string[] args)
         {
-            //lock (baton)
-            bool lockTaken = false;
-            Monitor.Enter(baton, ref lockTaken);
-            try
-            {
-                Console.WriteLine("Got the baton");
-            }
-            finally
-            {
-                if (lockTaken)
-                    Monitor.Exit(baton);
-            }
+            GenerateInts();
+            Console.WriteLine("Summing...");
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            long total = values.Aggregate<byte, long>(0, (current, t) => current + t);
+            watch.Stop();
+            Console.WriteLine("Total value is: " + total);
+            Console.WriteLine("Time to sum: " + watch.Elapsed);
         }
     }
 }
