398c1ba88b71446de5aa1ac11391415c3a372242
mgalarowicz <rezi610@gmail.com>
2020-08-19 06:51:46 +0200

    Synchronized containers

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,51 +1,118 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Threading;
 
 namespace Threading
 {
-    class BathroomStall
+    //In threading two separte operations (non atomic) are always a Danger Zone! For example:
+    //if (numbers.Count != 0)
+    //{
+    //      RIGHT HERE, DANGER ZONE!
+    //    numToSum = numbers.Dequeue();
+    //}
+
+class Program
     {
-        public void BeUsed(int userNumber)
+        static MySynchronizedQueue<int> numbers = new MySynchronizedQueue<int>();
+        static Random rand = new Random(999);
+        const int NumThreads = 3;
+        static int[] sums = new int[NumThreads];
+
+        static void ProduceNumbers()
         {
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("Being used by " + userNumber);
-                Thread.Sleep(500);
+                int numToEnqueue = rand.Next(10);
+                Console.WriteLine($"Producing thread adding {numToEnqueue} to the queue.");
+                //indirect way of access synchronization block (through an object on the heap)
+                lock (numbers)
+                    numbers.Enqueue(numToEnqueue);
+                Thread.Sleep(rand.Next(1000));
             }
+        }
+
+        static void SumNumbers(object threadNumber)
+        {
+            DateTime startTime = DateTime.Now;
+            int mySum = 0;
+            while ((DateTime.Now - startTime).Seconds < 11)
+            {
+                int numToSum = -1;
+
+                lock (numbers.SyncRoot)
+                {
+                    if (numbers.Count != 0)
+                    {
+                        numToSum = numbers.Dequeue();
+                    }
+                }
+
+                if (numToSum > -1)
+                {
+                    mySum += numToSum;
+                    Console.WriteLine($"Consuming thread #{threadNumber} adding {numToSum} to its total sum" +
+                                      $"making {mySum} for the thread total.");
+                }
+            }
+            sums[(int)threadNumber] = mySum;
+        }
+
+        static void Main(string[] args)
+        {
+            var producingThread = new Thread(ProduceNumbers);
+            producingThread.Start();
+            Thread[] threads = new Thread[NumThreads];
+
+            for (int i = 0; i < NumThreads; i++)
+            {
+                threads[i] = new Thread(SumNumbers);
+                threads[i].Start(i);
+            }
+
+            for (int i = 0; i < NumThreads; i++)
+            {
+                threads[i].Join();
+            }
+
+            int totalSum = 0;
+            for (int i = 0; i < NumThreads; i++)
+            {
+                totalSum += sums[i];
+            }
+
+            Console.WriteLine($"Done adding. Total is {totalSum}");
+
         }
     }
 
-    class Program
+    class MySynchronizedQueue<T>
     {
-        static BathroomStall stall = new BathroomStall();
+        object baton = new object();
+        Queue<T> theQ = new Queue<T>();
 
-        static void Main()
+        public void Enqueue(T item)
         {
-            for (int i = 0; i < 3; i++)
-            {
-                new Thread(RegularUsers).Start();
+            lock(baton)
+                theQ.Enqueue(item);
+        }
+
+        public T Dequeue()
+        {
+            lock (baton)
+                return theQ.Dequeue();
+        }
+
+        public int Count
+        {
+            get { 
+            lock (baton)
+                return theQ.Count;
             }
-            new Thread(TheWeirdGuy).Start();
         }
-
-        static void RegularUsers()
+        public object SyncRoot
         {
-            lock (stall)
-                stall.BeUsed(Thread.CurrentThread.ManagedThreadId);
+            get { return baton; }
         }
-
-        private static void TheWeirdGuy()
-        {
-            //lock on a object - it's not locking the object. What it is really locking on is CLR resource
-            //called synchronization block (structures that CLR actually uses to enforce mutual exclusion between all the threads
-            Monitor.Enter(stall); 
-            stall.BeUsed(99);
-            Monitor.Exit(stall);
-        }
-
     }
 }
+
