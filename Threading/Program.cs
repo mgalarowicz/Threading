17a02a164f3ad4a77612cacb4c4c7c0823c7b49e
mgalarowicz <rezi610@gmail.com>
2020-08-19 07:30:16 +0200

    Why locking on this is bad

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -4,29 +4,48 @@ using System.Threading;
 
 namespace Threading
 {
+    class BathroomStall
+    { 
+    object baton = new object();
+
+        public void BeUsed()
+        {
+            //lock (this)  if i lock on this, then for example if I lock reference to an object BathroomStall (outside) in method
+            //CleanSink then I will block to use my methods BeUsed and FlushToilet, and that do not make sense. Thats why we used baton
+            lock (baton)
+                Console.WriteLine("Doing our thing...");
+        }
+        public void FlushToilet()
+        {
+            lock (baton)
+                Console.WriteLine("Flushing the toilet...");
+        }
+    }
+
     class PublicRestroom
     {
-        //using the same baton on two stalls blocks at the same time both, and that don't make sense
-        object baton = new object();
-        object stall1baton = new object();
-        object stall2baton = new object();
+        BathroomStall stall1 = new BathroomStall();
+        BathroomStall stall2 = new BathroomStall();
 
         public void UseStall1()
         {
-            lock (stall1baton)
+            lock (stall1)
             {
-                Console.WriteLine("In stall 1");
-                Thread.Sleep(2000);
+                stall1.BeUsed();
+                stall1.FlushToilet();
             }
         }
 
         public void UseStall2()
         {
-            lock (stall2baton)
-            {
-                Console.WriteLine("In stall 2");
-                Thread.Sleep(2000);
-            }
+            stall2.BeUsed();
+            stall2.FlushToilet();
+        }
+
+        public void CleanSink()
+        {
+            lock (stall1)
+                Console.WriteLine("Cleaning sink...");
         }
     }
     class Program
